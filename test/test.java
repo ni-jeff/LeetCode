0b679729c6ef2a47bf1cfa2ef66aa99e69eef25e
ni_jeff <nixiyin_jeff@163.com>
2020-12-21 21:55:01 +0800

    q39

--- test/test.java
+++ test/test.java
@@ -8,33 +8,40 @@ import java.util.*;
 public class test{
     public static void main(String[] args) {
         Solution solution=new Solution();
-        int nums[]={1};
-        solution.nextPermutation(nums);
+        int nums[]={2,3,2,2,7};
+        List<List<Integer>> list=new ArrayList<>();
+        list=solution.combinationSum(nums,7);
         System.out.println(nums);
     }
 }
 class Solution {
-    public void nextPermutation(int[] nums) {
-        int i=nums.length-1;
-        // if(i<1)return;
-        while(i>0&&nums[i]<=nums[i-1]){i--;}
-        int end=nums.length-1;
-        for(int j=end;i>0&&j>=i;j--){
-            if(nums[j]>nums[i-1]){
-                swap(nums,j,i-1);
-                break;
-            }
-        }
-        while(end-i>0){
-            swap(nums, end, i);
-            end--;
-            i++;
-        }
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans=new ArrayList<>();
+        Arrays.sort(candidates);
+        System.out.println(candidates);
+        backtrace(ans, new ArrayList<Integer>(), candidates, target, 0);
+
+        return ans;
     }
-    private void swap(int[] nums,int i,int j){
-        int temp=nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
+
+    private void backtrace(List<List<Integer>> List,List<Integer> tempList,int[] nums,int target,int start){
+        if(target<0){
+            return;
+        }
+
+        else if(target==0){
+            List.add(new ArrayList<>(tempList));
+        }
+
+        else if(target>0){
+
+            for(int i=start;i<nums.length;i++){
+                tempList.add(nums[i]);
+                backtrace(List, tempList, nums, target-nums[i], start);
+                tempList.remove(tempList.size()-1);
+            }
+
+        }
     }
 }
 
