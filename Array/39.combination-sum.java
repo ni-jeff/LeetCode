0705e598f925736a3041c256ee8bcd66d7167e8a
ni_jeff <nixiyin_jeff@163.com>
2020-12-25 00:19:36 +0800

    q39 left dp solution

--- Array/39.combination-sum.java
+++ Array/39.combination-sum.java
@@ -5,71 +5,61 @@ import java.util.*;
 * [39] Combination Sum
 */
 
+import javax.lang.model.element.Element;
+
 // @lc code=start
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> ans=new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(candidates);
-        
         backtrack(ans, new ArrayList<Integer>(), candidates, target, 0);
 
         return ans;
     }
 
-    private void backtrack(List<List<Integer>> List,List<Integer> tempList,int[] nums,int target,int start){
-        if(target<0){
+    private void backtrack(List<List<Integer>> List, List<Integer> tempList, int[] nums, int target, int start) {
+        if (target < 0) {
             return;
         }
 
-        else if(target==0){
+        else if (target == 0) {
             List.add(new ArrayList<>(tempList));
         }
 
-        else if(target>0){
+        else if (target > 0) {
 
-            for(int i=start;i<nums.length;i++){
+            for (int i = start; i < nums.length; i++) {
                 tempList.add(nums[i]);
-                backtrack(List, tempList, nums, target-nums[i], i);
-                tempList.remove(tempList.size()-1);
+                backtrack(List, tempList, nums, target - nums[i], i);
+                tempList.remove(tempList.size() - 1);
             }
 
         }
     }
 }
 // @lc code=end
-/* solution:
-    1.backtrack:回溯要记住的点在于找到求解方法中哪个过程的重复的，找到边界点和全局变量;
-    ***************************************************************************
-            class Solution {
-                public List<List<Integer>> combinationSum(int[] candidates, int target) {
-                    List<List<Integer>> ans=new ArrayList<>();
-                    Arrays.sort(candidates);
-
-                    backtrack(ans, new ArrayList<Integer>(), candidates, target, 0);
-
-                    return ans;
-                }
-
-                private void backtrack(List<List<Integer>> List,List<Integer> tempList,int[] nums,int target,int start){
-                    if(target<0){
-                        return;
-                    }
-
-                    else if(target==0){
-                        List.add(new ArrayList<>(tempList));
-                    }
-
-                    else if(target>0){
-
-                        for(int i=start;i<nums.length;i++){
-                            tempList.add(nums[i]);
-                            backtrack(List, tempList, nums, target-nums[i], i);
-                            tempList.remove(tempList.size()-1);
-                        }
-
-                    }
-                }
-            }
-    ****************************************************************************
-    2.
-*/
+/*
+ * solution: 1.backtrack:回溯要记住的点在于找到求解方法中哪个过程的重复的，找到边界点和全局变量;
+ ***************************************************************************
+ * class Solution { public List<List<Integer>> combinationSum(int[] candidates,
+ * int target) { List<List<Integer>> ans=new ArrayList<>();
+ * Arrays.sort(candidates);
+ * 
+ * backtrack(ans, new ArrayList<Integer>(), candidates, target, 0);
+ * 
+ * return ans; }
+ * 
+ * private void backtrack(List<List<Integer>> List,List<Integer> tempList,int[]
+ * nums,int target,int start){ if(target<0){ return; }
+ * 
+ * else if(target==0){ List.add(new ArrayList<>(tempList)); }
+ * 
+ * else if(target>0){
+ * 
+ * for(int i=start;i<nums.length;i++){ tempList.add(nums[i]); backtrack(List,
+ * tempList, nums, target-nums[i], i); tempList.remove(tempList.size()-1); }
+ * 
+ * } } }
+ ****************************************************************************
+ * 2.dp
+ */
